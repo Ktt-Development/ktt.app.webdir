8b805eebe6f4016ce9a7c50fc033bd808ed5bc99
Katsute <Katsute@users.noreply.github.com>
2020-03-24 23:03:05 -0400

    Added imports

---
+++ src/main/java/com/kttdevelopment/webdir/Main.java
@@ -0,0 +1,15 @@
+package com.kttdevelopment.webdir;
+
+import com.kttdevelopment.simplehttpserver.SimpleHttpServer;
+
+public class Main {
+
+    private static SimpleHttpServer server;
+
+    public static void main(String[] args){
+
+        
+
+    }
+
+}
