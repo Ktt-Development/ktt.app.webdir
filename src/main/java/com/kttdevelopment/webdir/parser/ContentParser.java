f3c0c79aeb8a361a742c20547f44cb81af541f32
Katsute <Katsute@users.noreply.github.com>
2020-05-08 16:18:32 -0400

    snap

--- src/main/java/com/kttdevelopment/webdir/parser/ContentParser.java
+++
@@ -1,9 +0,0 @@
-package com.kttdevelopment.webdir.parser;
-
-import java.util.Map;
-
-public abstract class ContentParser {
-
-    public abstract String parse(final String content, final Map config);
-
-}
