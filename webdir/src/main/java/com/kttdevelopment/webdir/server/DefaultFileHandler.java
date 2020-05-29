b8d08b97f9aa0cbfa1e774fd5ff714585d743355
Katsute <Katsute@users.noreply.github.com>
2020-05-28 21:43:09 -0400

    Implement front matter

--- webdir/src/main/java/com/kttdevelopment/webdir/server/DefaultFileHandler.java
+++ webdir/src/main/java/com/kttdevelopment/webdir/server/DefaultFileHandler.java
@@ -2,24 +2,25 @@ package com.kttdevelopment.webdir.server;
 
 import com.kttdevelopment.simplehttpserver.SimpleHttpExchange;
 import com.kttdevelopment.simplehttpserver.handler.FileHandler;
+import com.kttdevelopment.webdir.api.formatter.YamlFrontMatter;
+import com.kttdevelopment.webdir.api.serviceprovider.ConfigurationSection;
+import com.kttdevelopment.webdir.formatter.YamlFrontMatterReader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 public class DefaultFileHandler extends FileHandler {
-
-    private static final Pattern fm = Pattern.compile("^(---)(.*)(---)$",Pattern.DOTALL | Pattern.MULTILINE);
-
-
 
     @Override
     public void handle(final SimpleHttpExchange exchange, final File source, final byte[] bytes) throws IOException{
         final String str = new String(bytes);
 
-        if(fm.matcher(str).matches()){
+        final YamlFrontMatter frontMatter = new YamlFrontMatterReader(str).read();
 
-            // use formatter
+        if(frontMatter.hasFrontMatter()){
+            final ConfigurationSection config = frontMatter.getFrontMatter();
+
+            // use formatter & send finished content w/o front matter
         }else{
             // send literal
             super.handle(exchange, source, bytes);
