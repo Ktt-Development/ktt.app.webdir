3e097cf13800a286a37ddc7f81b8ae88193f6a47
Katsute <Katsute@users.noreply.github.com>
2020-05-28 21:11:44 -0400

    Drop flexmark

--- webdir/src/main/java/com/kttdevelopment/webdir/server/DefaultFileHandler.java
+++ webdir/src/main/java/com/kttdevelopment/webdir/server/DefaultFileHandler.java
@@ -5,12 +5,25 @@ import com.kttdevelopment.simplehttpserver.handler.FileHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class DefaultFileHandler extends FileHandler {
 
+    private static final Pattern fm = Pattern.compile("^(---)(.*)(---)$",Pattern.DOTALL | Pattern.MULTILINE);
+
+
+
     @Override
     public void handle(final SimpleHttpExchange exchange, final File source, final byte[] bytes) throws IOException{
-        super.handle(exchange, source, bytes);
+        final String str = new String(bytes);
+
+        if(fm.matcher(str).matches()){
+
+            // use formatter
+        }else{
+            // send literal
+            super.handle(exchange, source, bytes);
+        }
     }
 
 }
