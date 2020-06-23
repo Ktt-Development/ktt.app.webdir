bd8e4702f762ba04463c83c90c162d9f26d13539
Katsute <Katsute@users.noreply.github.com>
2020-06-23 15:41:20 -0400

    Added plugin documentation

--- webdir-api/src/main/java/com/kttdevelopment/webdir/api/WebDirPlugin.java
+++ webdir-api/src/main/java/com/kttdevelopment/webdir/api/WebDirPlugin.java
@@ -9,18 +9,47 @@ import java.util.concurrent.ConcurrentHashMap;
 @SuppressWarnings("SpellCheckingInspection")
 public class WebDirPlugin {
 
-    // formatters
+// formatters
 
     private final Map<String, Map.Entry<Formatter,String>> formatters = new ConcurrentHashMap<>();
 
+    /**
+     * Returns an umodifiable map of the currently added formatters. <b>Reserved for WebDir</b>
+     *
+     * @return map of formatters.
+     *
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public final Map<String, Map.Entry<Formatter,String>> getFormatters(){
         return Collections.unmodifiableMap(formatters);
     }
 
+    /**
+     * Adds a formatter to the plugin.
+     *
+     * @param name name of formatter
+     * @param formatter formatter
+     *
+     * @see #addFormatter(String, Formatter, String)
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public synchronized final void addFormatter(final String name, final Formatter formatter){
         addFormatter(name,formatter,"");
     }
 
+    /**
+     * Adds a formatter to the plugin.
+     *
+     * @param name name of formatter
+     * @param formatter formatter
+     * @param permission permission required to use formatter
+     *
+     * @see #addFormatter(String, Formatter)
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public synchronized final void addFormatter(final String name, final Formatter formatter, final String permission){
         formatters.put(name, new Map.Entry<>() {
 
@@ -41,39 +70,94 @@ public class WebDirPlugin {
         });
     }
 
-    // handlers
+// handlers
+
 
     private final Map<SimpleFileHandler,String> handlers = new ConcurrentHashMap<>();
 
+    /**
+     * Returns an umodifiable map of the currently added handlers. <b>Reserved for WebDir</b>
+     *
+     * @return map of handlers
+     *
+     * @see SimpleFileHandler
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public final Map<SimpleFileHandler,String> getHandlers(){
         return Collections.unmodifiableMap(handlers);
     }
 
+    /**
+     * Adds a file handler to the plugin.
+     *
+     * @param handler handler
+     *
+     * @see #addHandler(SimpleFileHandler, String)
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public synchronized final void addHandler(final SimpleFileHandler handler){
         addHandler(handler,"");
     }
 
+    /**
+     * Adds a file handler to the plugin.
+     *
+     * @param handler handler
+     * @param permission permission required to use handler
+     *
+     * @see #addHandler(SimpleFileHandler)
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     public synchronized final void addHandler(final SimpleFileHandler handler, final String permission){
         handlers.put(handler,permission);
     }
 
-    // instance +pluginService
+// instance +pluginService
 
     private final PluginService pluginService;
 
+    /**
+     * Instantiates a plugin. <b>Reserved for WebDir</b>
+     *
+     * @param pluginService plugin service implementation
+     */
     public WebDirPlugin(final PluginService pluginService){
         this.pluginService = pluginService;
     }
 
+    /**
+     * Returns the WebDir API Implementation.
+     *
+     * @return plugin service
+     *
+     * @see PluginService
+     * @since 01.00.00
+     * @author Kt tDevelopment
+     */
     public final PluginService getPluginService(){
         return pluginService;
     }
 
-    // override methods
+// override methods
 
+    /**
+     * Handles when the plugin is enabled.
+     *
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     @SuppressWarnings("EmptyMethod")
     public void onEnable(){}
 
+    /**
+     * Handles when the plugin is disabled.
+     *
+     * @since 01.00.00
+     * @author Ktt Development
+     */
     @SuppressWarnings("EmptyMethod")
     public void onDisable(){}
 
