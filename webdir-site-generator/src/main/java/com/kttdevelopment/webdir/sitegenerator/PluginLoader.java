ef6e01ccaaaf93b3822d28d22d28aa9bd775d8f6
Katsute <Katsute@users.noreply.github.com>
2020-07-06 22:45:08 -0400

    Replicate plugin loader

--- webdir-site-generator/src/main/java/com/kttdevelopment/webdir/sitegenerator/PluginLoader.java
+++ webdir-site-generator/src/main/java/com/kttdevelopment/webdir/sitegenerator/PluginLoader.java
@@ -2,22 +2,53 @@ package com.kttdevelopment.webdir.sitegenerator;
 
 import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
+import com.kttdevelopment.webdir.api.PluginService;
 import com.kttdevelopment.webdir.api.WebDirPlugin;
 import com.kttdevelopment.webdir.api.serviceprovider.ConfigurationSection;
 import com.kttdevelopment.webdir.sitegenerator.config.ConfigurationSectionImpl;
 import com.kttdevelopment.webdir.sitegenerator.function.Exceptions;
+import com.kttdevelopment.webdir.sitegenerator.function.TriConsumer;
+import com.kttdevelopment.webdir.sitegenerator.object.TriTuple;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiConsumer;
 import java.util.logging.Logger;
 
 public final class PluginLoader {
 
     private static final String mainClassName = "main";
+
+    protected TriConsumer<File,Class<WebDirPlugin>,ConfigurationSection> loader = (pluginFile, pluginClass, yml) -> {
+        try{
+            final LocaleService locale = Main.getLocaleService();
+
+            final PluginService provider = null; // ← require plugin var checks in here
+            final String pluginName = provider.getPluginName();
+
+            final Logger logger = Logger.getLogger(pluginName);
+
+            try{
+                final WebDirPlugin plugin = pluginClass.getDeclaredConstructor(PluginService.class).newInstance(provider);
+                plugin.onEnable();
+            }catch(final InstantiationException e){
+                logger.severe(locale.getString("pluginLoader.loader.abstract", pluginName));
+            }catch(final IllegalAccessException e){
+                logger.severe(locale.getString("pluginLoader.loader.scope", pluginName));
+            }catch(final NoSuchMethodException | IllegalArgumentException ignored){
+                logger.severe(locale.getString("pluginLoader.loader.constArgs", pluginName));
+            }catch(final ExceptionInInitializerError | InvocationTargetException e){
+                logger.severe(locale.getString("pluginLoader.loader.const", pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
+            }catch(final SecurityException e){
+                logger.severe(locale.getString("pluginLoader.loader.sec", pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
+            }
+        }catch(final Exception e){
+            throw new RuntimeException(e);
+        }
+    };
 
     @SuppressWarnings("unchecked")
     PluginLoader(){
@@ -25,7 +56,7 @@ public final class PluginLoader {
         final ConfigService config = Main.getConfigService();
         final Logger logger = Logger.getLogger(locale.getString("pluginLoader"));
 
-        logger.info(locale.getString("pluginService.const"));
+        logger.info(locale.getString("pluginLoader.const"));
 
         // config pluginFolder
         final File pluginFolder = new File("/plugins");
@@ -33,8 +64,7 @@ public final class PluginLoader {
         // config safemode
 
         // load jar files
-        final List<URL> pluginURLs = new ArrayList<>();
-        final int totalPlugins = pluginURLs.size();
+        final Map<File,URL> pluginJars = new HashMap<>();
         {
             final File[] plugins =  Objects.requireNonNullElse(
                 pluginFolder.listFiles((dir, name) -> !dir.isDirectory() && name.toLowerCase().endsWith(".jar")),
@@ -42,82 +72,103 @@ public final class PluginLoader {
             );
             for(final File file : plugins){
                 try{
-                    pluginURLs.add(file.toURI().toURL());
-                }catch(final MalformedURLException e){
-                    logger.severe(locale.getString("pluginService.const.badURL", file.getName() + '\n' + Exceptions.getStackTraceAsString(e)));
+                    pluginJars.put(file,file.toURI().toURL());
+                }catch(final MalformedURLException | IllegalArgumentException e){
+                    logger.severe(locale.getString("pluginLoader.const.badURL", file.getName() + '\n' + Exceptions.getStackTraceAsString(e)));
                 }
             }
         }
+        final int initialPluginCount = pluginJars.size();
 
         // load plugin.yml
-        final Enumeration<URL> resourceURLs;
-        final URLClassLoader loader = new URLClassLoader(pluginURLs.toArray(new URL[0]));
-        {
-            try{
-                resourceURLs = loader.findResources("plugin.yml");
+        final Map<File,URL> pluginYMLs = new HashMap<>();
+        pluginJars.forEach((file, url) -> {
+            try(final URLClassLoader loader = new URLClassLoader(new URL[]{url})){
+                final URL yml = Objects.requireNonNull(loader.findResource("plugin.yml"));
+                pluginYMLs.put(file,yml);
+            }catch(final SecurityException e){
+                logger.severe(locale.getString("pluginLoader.const.UCLSec",file.getName()) + '\n' + Exceptions.getStackTraceAsString(e));
+            }catch(final NullPointerException ignored){
+                logger.severe(locale.getString("pluginLoader.const.nullYML",file.getName()));
             }catch(final IOException e){
-                logger.severe(locale.getString("pluginService.const.pluginURLs") + '\n' + Exceptions.getStackTraceAsString(e));
-                try{ loader.close(); // close if fails here
-                }catch(IOException ignored){ }
-                return;
+                logger.warning(locale.getString("pluginLoader.const.UCLCloseIO", file.getName()) + '\n' + Exceptions.getStackTraceAsString(e));
             }
-        }
+        });
 
         // load plugin main
-        final Map<Class<WebDirPlugin>,ConfigurationSection> plugins = new LinkedHashMap<>();
-        while(resourceURLs.hasMoreElements()){
-            // locate main
-            final URL ymlURL = resourceURLs.nextElement();
-            final String ymlURLstr = ymlURL.toString();
+        final List<TriTuple<File,Class<WebDirPlugin>,ConfigurationSection>> plugins = new ArrayList<>();
+
+        pluginYMLs.entrySet().removeIf(entry -> {
+            final File plugin = entry.getKey();
+            final String pluginName = plugin.getName();
+            final URL ymlURL = entry.getValue();
+
             final ConfigurationSection yml;
 
             YamlReader IN = null;
             try{
                 IN = new YamlReader(new InputStreamReader(ymlURL.openStream()));
+                //noinspection rawtypes
                 yml = new ConfigurationSectionImpl((Map) IN.read());
             }catch(final ClassCastException | YamlException e){
-                logger.warning(locale.getString("pluginService.const.badYml",ymlURLstr)+ '\n' + Exceptions.getStackTraceAsString(e));
-                continue;
+                logger.severe(locale.getString("pluginLoader.const.badYMLSyntax",pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
+                return true;
             }catch(final IOException e){
-                logger.warning(locale.getString("pluginService.const.badYmlIO",ymlURLstr) + '\n' + Exceptions.getStackTraceAsString(e));
-                continue;
+                logger.severe(locale.getString("pluginLoader.const.ymlStreamIO", pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
+                return true;
             }finally{
                 if(IN != null)
                     try{ IN.close();
                     }catch(final IOException e){
-                        logger.warning(locale.getString("pluginService.const.badCloseIO",ymlURLstr) + '\n' + Exceptions.getStackTraceAsString(e));
+                        logger.warning(locale.getString("pluginLoader.const.streamClose",pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
                     }
             }
 
-            // load main
-            try{
-                plugins.put((Class<WebDirPlugin>) loader.loadClass(Objects.requireNonNull(mainClassName)),yml);
+            try(final URLClassLoader loader = new URLClassLoader(new URL[]{plugin.toURI().toURL()})){
+                plugins.add(new TriTuple<>(plugin,(Class<WebDirPlugin>) loader.loadClass(Objects.requireNonNull(mainClassName)),yml));
+            }catch(MalformedURLException | IllegalArgumentException e){
+                logger.severe(locale.getString("pluginLoader.const.UCLSec",pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
+                return true;
             }catch(final ClassNotFoundException | NullPointerException ignored){
-                logger.warning(locale.getString("pluginService.const.missingMain",ymlURLstr));
-            }catch(final ClassCastException e){
-                logger.warning(locale.getString("pluginService.const.badMainCast",ymlURLstr) + '\n' + Exceptions.getStackTraceAsString(e));
+                logger.severe(locale.getString("pluginLoader.const.noMainClass",pluginName));
+                return true;
+            }catch(final ClassCastException ignored){
+                logger.severe(locale.getString("pluginLoader.const.badMainCast",pluginName));
+                return true;
+            }catch(final IOException e){
+                logger.warning(locale.getString("pluginLoader.const.UCLCloseIO", pluginName) + '\n' + Exceptions.getStackTraceAsString(e));
             }
-        }
+
+            return false;
+        });
 
         // initialize plugins
         final ExecutorService executor = Executors.newSingleThreadExecutor();
 
         final AtomicInteger loadedPlugins = new AtomicInteger(0);
-        plugins.forEach((mainClass, pluginYml) -> {
-            final Future<?> future = executor.submit(() -> {
+        final int timeout = 30;
+        final TimeUnit unit = TimeUnit.SECONDS;
 
-                loadedPlugins.incrementAndGet();
-            });
+        plugins.forEach(tuple -> {
+            final File plugin = tuple.getVar1();
+            final Class<WebDirPlugin> mainClass = tuple.getVar2();
+            final ConfigurationSection yml = tuple.getVar3();
+
+            final Future<?> future = executor.submit(() -> loader.consume(plugin, mainClass, yml));
 
             try{
-                future.get(30,TimeUnit.SECONDS);
+                future.get(timeout,unit);
             }catch(final Exception e){
                 future.cancel(true);
-
+                if(e instanceof TimeoutException)
+                    logger.severe(locale.getString("pluginLoader.loader.timedOut",plugin.getName(),timeout + ' ' + unit.name().toLowerCase()));
+                else
+                    logger.severe(locale.getString("pluginLoader.loader.unknown",plugin.getName()) + '\n' + Exceptions.getStackTraceAsString(e));
             }
         });
 
+
         executor.shutdown();
-        logger.info(locale.getString("pluginService.const.loaded",loadedPlugins.get(),totalPlugins));
+        logger.info(locale.getString("pluginLoader.const.loaded",loadedPlugins.get(),initialPluginCount));
     }
 }
---
+++ webdir-site-generator/src/main/java/com/kttdevelopment/webdir/sitegenerator/function/TriConsumer.java
@@ -0,0 +1,7 @@
+package com.kttdevelopment.webdir.sitegenerator.function;
+
+public interface TriConsumer<X,Y,Z> {
+
+    void consume(X x, Y y, Z z);
+
+}
