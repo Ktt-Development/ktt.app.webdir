551b4f55ac7d317d937745855533d84c4e319d5f
Katsute <Katsute@users.noreply.github.com>
2020-07-09 17:38:12 -0400

    Create ConfigurationFileTests.java

---
+++ webdir-generator/src/test/java/ConfigurationFileTests.java
@@ -0,0 +1,21 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ConfigurationFileTests {
+
+    @Test @Ignore
+    public void testDefault(){
+
+    }
+
+    @Test @Ignore
+    public void testReload(){
+
+    }
+
+    @Test @Ignore
+    public void testLoad(){
+
+    }
+
+}
