ba54652914e2a59c23bbb002c53907d805e68eea
Katsute <58778985+Katsute@users.noreply.github.com>
2020-08-11 15:22:23 -0400

    Add deployment 🎉

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+SET version=R-EAV-02
+
+CALL :build Generator, generator
+CALL :build Server, server
+
+EXIT /B
+
+:build
+jpackage --name "WebDir - %~1" ^
+--input webdir-%~2/target/jar ^
+--main-jar webdir-%~2-%version%-jar-with-dependencies.jar ^
+--dest deploy ^
+--type app-image ^
+--icon branding/Logo.ico ^
+--copyright "Ktt Development 2020"
