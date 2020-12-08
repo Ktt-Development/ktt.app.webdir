a5ed682e4da1bd9c45b6eca1f2a590e90448deef
Katsute <58778985+Katsute@users.noreply.github.com>
2020-12-08 17:14:06 -0500

    remove project modularity

--- build.sh
+++ build.sh
@@ -6,16 +6,14 @@ Dest="WebDir"
 
 mkdir -p $Workspace
 
-cp modules/webdir-client-$Version.jar $Workspace/webdir-client-$Version.jar
-
 jpackage \
 --name "WebDir" \
 --icon icon.ico \
 --input $Workspace \
 --dest . \
 --type app-image \
---module-path "modules" \
---module webdir.client \
+--main-jar webdir-client-$Version.jar \
+--main-class com.kttdevelopment.webdir.client.Main \
 --app-version $Version \
 --vendor "$Vendor" \
 --copyright "Copyright $Vendor 2020" \
